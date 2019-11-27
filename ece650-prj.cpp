4017c833dd4f2e776468378aeb634da66154c8bc
Hammad Rehman <harehman@uwaterloo.ca>
2019-11-26 22:57:18 -0500

    Laruain change 26th Nov

--- ece650-prj.cpp
+++ ece650-prj.cpp
@@ -15,37 +15,55 @@
 #include "graph.hpp"
 #include "cover.hpp"
 
-const int CNF_SAT_VC    = 0;
-const int APPROX_VC_1   = 1;
-const int APPROX_VC_2   = 2;
+const int WATCHDOG      = 0;
+const int CNF_SAT_VC    = 1;
+const int APPROX_VC_1   = 2;
+const int APPROX_VC_2   = 3;
+
+struct thread_context { 
+    // Context
+    pthread_t             thread[4] = {};
+    pthread_mutex_t         *mutex; 
+    int                      count;
+    // Input
+    uint                   timeout;
+    Graph                        g;
+};
+
 std::string ALGO[] = { "CNF-SAT-VC", "APPROX-VC-1", "APPROX-VC-2" };
 
-void *      cnf_sat_vc_wrapper    (void *arg);
-std::pair<std::vector<int>, double> cnf_sat_vc_impl(Graph& g, int k);
+void        cleanup_vc_thread(void* arg);
+void *        watchdog_thread(void *arg);
+void *      cnf_sat_vc_thread(void *arg);
+void *     approx_vc_1_thread(void *arg);
+void *     approx_vc_2_thread(void *arg);
 
-void *     approx_vc_1_wrapper    (void *arg);
+std::pair<std::vector<int>, double>  cnf_sat_vc_impl(Graph& g, int k);
 std::pair<std::vector<int>, double> approx_vc_1_impl(Graph& g, int k);
-
-void *     approx_vc_2_wrapper    (void *arg);
 std::pair<std::vector<int>, double> approx_vc_2_impl(Graph& g, int k);
 
-Graph                  read_in    ();
-std::array<std::pair<std::vector<int>, double>, 3> process_in_parallel(const Graph& g);
-void parse_arguments(int argc, char* argv[], bool&);
+Graph read_in();
+void parse_arguments(int argc, char* argv[], bool&, int& timeout);
+std::array<std::pair<std::vector<int>, double>, 3> process_in_parallel(const Graph& g, int timeout);
 
 int main(int argc, char** argv) {
 
     bool benchmark_mode = false;
-    parse_arguments(argc, argv, benchmark_mode);
+    
+    int  timeout_seconds = 120;
+    parse_arguments(argc, argv, benchmark_mode, timeout_seconds);
     
     while(!std::cin.eof()) {
         
-        Graph input = read_in();
-        std::array<std::pair<std::vector<int>, double>, 3> output = process_in_parallel(input);
+        Graph g = read_in();
+        std::array<std::pair<std::vector<int>, double>, 3> output = process_in_parallel(g, timeout_seconds);
         
         for(size_t i = 0; i < 3; i++) {
             if(benchmark_mode) {
-                std::cout << std::fixed << std::setprecision(2) << output[i].second;
+                if(output[i].second != -1)
+                    std::cout << std::fixed << std::setprecision(4) << output[i].second;
+                else
+                    std::cout << "timeout";
                 
                 if(i < 2)
                     std::cout << ",";
@@ -56,7 +74,10 @@ int main(int argc, char** argv) {
                 std::sort(output[i].first.begin(), output[i].first.end());
                 for(size_t j = 0; j < output[i].first.size(); j++) {
                     
-                    std::cout << output[i].first[j];
+                    if(output[i].second >= 0)
+                        std::cout << output[i].first[j];
+                    else
+                        std::cout << "timeout";
 
                     if (j < output[i].first.size() - 1)
                         std::cout << ",";
@@ -67,12 +88,15 @@ int main(int argc, char** argv) {
     }
 }
 
-void parse_arguments(int argc, char* argv[], bool& benchmark_mode) {
+void parse_arguments(int argc, char* argv[], bool& benchmark_mode, int& timeout_seconds) {
     char opt;
-    while((opt = getopt(argc, argv, "bo:")) != -1) {
+    while((opt = getopt(argc, argv, "bo:t:")) != -1) {
         switch(opt) {
             case 'b':
                 benchmark_mode = true;
+                break;
+            case 't':
+                timeout_seconds = std::stoi(optarg);
                 break;
             default:
                 break;
@@ -149,25 +173,34 @@ Graph read_in () {
     return g;
 }
 
-std::array<std::pair<std::vector<int>, double>, 3> process_in_parallel(const Graph& g) {
+std::array<std::pair<std::vector<int>, double>, 3> process_in_parallel(const Graph& g, int timeout) {
 
-    pthread_t  threads[3];
-    void* (*thread_run[])(void*) = { cnf_sat_vc_wrapper, approx_vc_1_wrapper, approx_vc_2_wrapper };
+    pthread_mutex_t mutex  = PTHREAD_MUTEX_INITIALIZER;
+
+    struct thread_context ctx;
+    ctx.mutex = &mutex;
+    ctx.timeout = timeout;
+    ctx.g = g;
     
-    for (int i = 0; i < 3; i++) {
-        pthread_create(&threads[i], NULL, thread_run[i], (void *)&g);
+    void* (*thread_run[])(void*) = { watchdog_thread, cnf_sat_vc_thread, approx_vc_1_thread, approx_vc_2_thread };
+    for (int i = 0; i < 4; i++) {
+        pthread_create(&ctx.thread[i], NULL, thread_run[i], (void *)&ctx);
     }
 
     std::array<std::pair<std::vector<int>, double>, 3> output = {};
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
         
         void * retptr;
-        pthread_join(threads[i], &retptr);
+        pthread_join(ctx.thread[i], &retptr);
         
-        std::pair<std::vector<int>, double> *resultptr = static_cast<std::pair<std::vector<int>, double>*>(retptr);
-        output[i] = *resultptr;
-           
-        delete resultptr;
+        if(i >= 1 && retptr == PTHREAD_CANCELED) {
+            output[i - 1] = std::make_pair(std::vector<int>(), -1); 
+        }
+        else {
+            std::pair<std::vector<int>, double> *resultptr = static_cast<std::pair<std::vector<int>, double>*>(retptr);
+            output[i - 1] = *resultptr;
+            delete resultptr;
+        }
     }
 
     return output;
@@ -190,24 +223,30 @@ std::vector<int> cnf_sat_vc_impl(Graph& g) {
     return std::vector<int>{};
 }
 
-void * cnf_sat_vc_wrapper (void *arg) {
+void * cnf_sat_vc_thread (void *arg) {
 
-    Graph *g = (Graph *)arg;
+    struct thread_context *ctx = (struct thread_context *)arg;
     std::pair<std::vector<int>, double> * output = new std::pair<std::vector<int>, double>{};
+    pthread_cleanup_push(cleanup_vc_thread, output);
     
     clockid_t cid;
+    struct timespec t1, t2;
     pthread_getcpuclockid(pthread_self(), &cid);
     
-    struct timespec t1, t2;
     clock_gettime(cid, &t1);
-
-    output->first  = cnf_sat_vc_impl(*g);
-    
+    output->first  = cnf_sat_vc_impl(ctx->g);
     clock_gettime(cid, &t2);
-
     output->second = tdiff(t2, t1);
 
-    return output;
+    pthread_mutex_lock(ctx->mutex);
+    ctx->count += 1;
+    if(ctx->count == 3) {
+        pthread_cancel(ctx->thread[WATCHDOG]);
+    }
+    pthread_mutex_unlock(ctx->mutex);
+
+    pthread_cleanup_pop(0);
+    pthread_exit(output);
 }
 
 
@@ -222,26 +261,32 @@ std::vector<int> approx_vc_1_impl(Graph& g) {
         return std::vector<int>{};
 }
 
-void * approx_vc_1_wrapper (void *arg) {
+void * approx_vc_1_thread (void *arg) {
 
-    Graph *g = (Graph *)arg;
+    struct thread_context *ctx = (struct thread_context*)arg;
     std::pair<std::vector<int>, double> * output = new std::pair<std::vector<int>, double>{};
     
+    pthread_cleanup_push(cleanup_vc_thread, output);
+
     clockid_t cid;
+    struct timespec t1, t2;
     pthread_getcpuclockid(pthread_self(), &cid);
     
-    struct timespec t1, t2;
     clock_gettime(cid, &t1);
-
-    output->first  = approx_vc_1_impl(*g);
-    
+    output->first  = approx_vc_1_impl(ctx->g);
     clock_gettime(cid, &t2);
-
     output->second = tdiff(t2, t1);
 
+    pthread_mutex_lock(ctx->mutex);
+    ctx->count += 1;
+    if(ctx->count == 3) {
+        pthread_cancel(ctx->thread[WATCHDOG]);
+    }
+    pthread_mutex_unlock(ctx->mutex);
+    
+    pthread_cleanup_pop(0);
     pthread_exit(output);
 }
-
 
 std::vector<int> approx_vc_2_impl(Graph& g) {
 
@@ -254,22 +299,50 @@ std::vector<int> approx_vc_2_impl(Graph& g) {
         return std::vector<int>{};
 }
 
-void * approx_vc_2_wrapper (void *arg) {
+void * approx_vc_2_thread (void *arg) {
     
-    Graph *g = (Graph *)arg;
+    struct thread_context *ctx = (struct thread_context*)arg;
     std::pair<std::vector<int>, double> * output = new std::pair<std::vector<int>, double>{};
-    
+    pthread_cleanup_push(cleanup_vc_thread, output);
+
     clockid_t cid;
+    struct timespec t1, t2;
     pthread_getcpuclockid(pthread_self(), &cid);
     
-    struct timespec t1, t2;
     clock_gettime(cid, &t1);
-
-    output->first = approx_vc_2_impl(*g);
-    
+    output->first = approx_vc_2_impl(ctx->g);
     clock_gettime(cid, &t2);
-    
     output->second = tdiff(t2, t1);
+
+    pthread_mutex_lock(ctx->mutex);
+    ctx->count += 1;
+    if(ctx->count == 3) {
+        pthread_cancel(ctx->thread[WATCHDOG]);
+    }
+    pthread_mutex_unlock(ctx->mutex);
+
+    pthread_cleanup_pop(0);
     pthread_exit(output);
 }
 
+void * watchdog_thread (void *arg) {
+    
+    struct thread_context *ctx = (struct thread_context *)arg;
+    sleep(ctx->timeout);
+
+    pthread_mutex_lock(ctx->mutex);
+    for(int i = 1; i < 4; i++)
+        pthread_cancel(ctx->thread[i]);
+    pthread_mutex_unlock(ctx->mutex);
+    
+    pthread_exit(NULL);
+}
+
+void cleanup_vc_thread(void *arg) {
+     
+    std::pair<std::vector<int>, double> *resultptr = 
+        static_cast<std::pair<std::vector<int>, double>*>(arg);
+
+    if(resultptr)
+        delete resultptr;
+}
