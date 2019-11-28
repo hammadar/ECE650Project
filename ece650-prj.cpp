421bb39df5f53e9c5908baa8f60267c4fbd07ea8
Hammad Rehman <harehman@uwaterloo.ca>
2019-11-27 19:10:18 -0500

    commented out cnf thread

--- ece650-prj.cpp
+++ ece650-prj.cpp
@@ -203,13 +203,13 @@ std::array<std::pair<std::vector<int>, double>, 3> process_in_parallel(const Gra
     ctx.count = 0;
     ctx.g = g;
     
-    void* (*thread_run[])(void*) = { watchdog_thread, cnf_sat_vc_thread, approx_vc_1_thread, approx_vc_2_thread };
-    for (int i = 0; i < 4; i++) {
+    void* (*thread_run[])(void*) = { watchdog_thread, /*cnf_sat_vc_thread,*/ approx_vc_1_thread, approx_vc_2_thread };
+    for (int i = 0; i < 3; i++) {
         pthread_create(&ctx.thread[i], NULL, thread_run[i], (void *)&ctx);
     }
 
     std::array<std::pair<std::vector<int>, double>, 3> output = {};
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 3; i++) {
         
         void * retptr;
         pthread_join(ctx.thread[i], &retptr);
