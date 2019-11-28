0f9594c95078d009e77e77fec3a91e15ccd1ab39
Hammad Rehman <h.ahmed.rehman@gmail.com>
2019-11-27 22:16:21 -0500

    script issues

--- script.sh
+++ script.sh
@@ -1,16 +1,29 @@
-#!/usr/bin/bash
+#!/bin/bash
 chmod u+x script.sh
 
-vertices=(5, 10, 15, 20, 25, 30, 35, 40, 45, 50)
+vertices=(5 10 15 20 25 30 35 40 45 50)
 
 for vertex in "${vertices[@]}"
 do
-	echo $vertex >> output.txt  
-
-	for ((counter=0; counter<10; counter++))
+	for ((graphcount=0; graphcount<10; graphcount++))
 	do
-		./graphGen $vertex | build/./ece650-prj -b -t 60 >> output.txt
+		echo $graphcount $vertex
+		echo Graph $graphcount >> output$vertex.txt
+		./graphGen $vertex > graph.txt
+		echo V$vertex G$graphcount >> graphs.txt
+		cat graph.txt >> graphs.txt 
 
+		for ((counter=0; counter<10; counter++))
+		do
+			graph.txt | build/./ece650-prj -b -t 60 >> output$vertex.txt
+		done
 	done
+	git add output$vertex.txt
 done
 
+rm graph.txt
+git add graphs.txt
+git commit -m "automated script adding outputs to git"
+
+
+return
